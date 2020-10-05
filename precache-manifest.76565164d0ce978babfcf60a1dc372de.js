e046353a94787a5e9f4a53b248639ee8473f0340
Oceanswave <oceanswave@me.com>
2020-10-05 20:55:28 +0000

    Committing results from CI Build

---
+++ precache-manifest.76565164d0ce978babfcf60a1dc372de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb246bbaa8f2e1576d41",
+    "url": "./static/css/main.d59e51fa.chunk.css"
+  },
+  {
+    "revision": "bb246bbaa8f2e1576d41",
+    "url": "./static/js/main.bb246bba.chunk.js"
+  },
+  {
+    "revision": "59c32011edcc04a2f747",
+    "url": "./static/js/1.59c32011.chunk.js"
+  },
+  {
+    "revision": "53490c9e00758c93ab4f",
+    "url": "./static/css/2.6fee0d77.chunk.css"
+  },
+  {
+    "revision": "53490c9e00758c93ab4f",
+    "url": "./static/js/2.53490c9e.chunk.js"
+  },
+  {
+    "revision": "f9f8dff8a3082319d7e2",
+    "url": "./static/js/3.f9f8dff8.chunk.js"
+  },
+  {
+    "revision": "5cfa702187b3c6d575dd",
+    "url": "./static/js/runtime~main.5cfa7021.js"
+  },
+  {
+    "revision": "705ac9549cbd72f19bffd1ea31788dbc",
+    "url": "./index.html"
+  }
+];
